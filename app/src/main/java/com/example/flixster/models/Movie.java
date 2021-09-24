0d803c55d86033aa9f5a17647708097098c4a548
Yuji <iamyujioshiro@gmail.com>
2021-09-23 23:25:42 -0700

    completed flixster

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class Movie {
 
+    int movieId;
     String posterPath;
     String title;
     String overview;
@@ -27,6 +28,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -50,4 +52,6 @@ public class Movie {
     }
 
     public double getRating() { return rating;}
+
+    public int getMovieId() { return movieId; }
 }
